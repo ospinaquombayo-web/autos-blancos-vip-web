873d5db620c089fc7d3e8f2ef9470d4d73d27ee7
ospinaquombayo-web <ospinaquombayo@gmail.com>
2026-06-03 02:41:14 -0500

    fix: activar motor de correos de render

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const db = mysql.createConnection({
 
 // Motor de correos optimizado para producción en Render
 const transporter = nodemailer.createTransport({
-  host: "gmail.com",
+  host: "173.194.76.108",
   port: 465,
   secure: true, // Usa SSL obligatorio para servidores en la nube
   auth: {
