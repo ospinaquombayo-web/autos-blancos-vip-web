ddcca3f300eb76f22b81c8328958f9238970f10e
ospinaquombayo-web <ospinaquombayo@gmail.com>
2026-06-03 01:30:19 -0500

    fix: forzar actualizacion de server a la nube

--- frontend/src/components/Formulario.jsx
+++ frontend/src/components/Formulario.jsx
@@ -26,7 +26,7 @@ function Formulario() {
     try {
       // 🚀 Ahora apuntamos directamente a tu servidor en vivo en internet
       const response = await fetch(
-        " https://autos-blancos-vip-web.onrender.com",
+        " https://autos-blancos-vip-web.onrender.com/cotizacion",
         {
           method: "POST",
           headers: {
